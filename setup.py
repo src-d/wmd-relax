78a97ae38f913eefc237da6a5e9e6f6e491396cb
Vadim Markovtsev <vadim@sourced.tech>
2017-03-22 11:00:53 +0100

    Add Python packaging

---
+++ setup.py
@@ -0,0 +1,86 @@
+from multiprocessing import cpu_count
+import os
+from setuptools import setup
+from setuptools.command.build_py import build_py
+from setuptools.dist import Distribution
+from shutil import copyfile
+from subprocess import check_call
+from sys import platform
+
+class SetupConfigurationError(Exception):
+    pass
+
+
+class CMakeBuild(build_py):
+    SHLIB = "libwmdrelax.so"
+
+    def run(self):
+        if not self.dry_run:
+            self._build()
+        super(CMakeBuild, self).run()
+
+    def get_outputs(self, *args, **kwargs):
+        outputs = super(CMakeBuild, self).get_outputs(*args, **kwargs)
+        outputs.extend(self._shared_lib)
+        return outputs
+
+    def _build(self, builddir=None):
+        if platform != "darwin":
+            cuda_toolkit_dir = os.getenv("CUDA_TOOLKIT_ROOT_DIR")
+            if cuda_toolkit_dir is None:
+                raise SetupConfigurationError(
+                    "CUDA_TOOLKIT_ROOT_DIR environment variable must be defined")
+            check_call(("cmake", "-DCMAKE_BUILD_TYPE=Release",
+                        "-DCUDA_TOOLKIT_ROOT_DIR=%s" % cuda_toolkit_dir, "."))
+        else:
+            ccbin = os.getenv("CUDA_HOST_COMPILER", "/usr/bin/clang")
+            env = dict(os.environ)
+            env.setdefault("CC", "/usr/local/opt/llvm/bin/clang")
+            env.setdefault("CXX", "/usr/local/opt/llvm/bin/clang++")
+            env.setdefault("LDFLAGS", "-L/usr/local/opt/llvm/lib/")
+            check_call(("cmake", "-DCMAKE_BUILD_TYPE=Release",
+                        "-DCUDA_HOST_COMPILER=%s" % ccbin, "-DSUFFIX=.so", "."),
+                       env=env)
+        check_call(("make", "-j%d" % cpu_count()))
+        self.mkpath(self.build_lib)
+        dest = os.path.join(self.build_lib, self.SHLIB)
+        copyfile(self.SHLIB, dest)
+        self._shared_lib = [dest]
+
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(self):
+        return True
+
+    def is_pure(self):
+        return False
+
+setup(
+    name="libwmdrelax",
+    description="Accelerated functions to calculate Word Mover's Distance",
+    version="1.0.0",
+    license="MIT",
+    author="source{d}",
+    author_email="vadim@sourced.tech",
+    url="https://github.com/src-d/wmd-relax",
+    download_url="https://github.com/src-d/wmd-relax",
+    py_modules=["libwmdrelax"],
+    install_requires=["numpy"],
+    distclass=BinaryDistribution,
+    cmdclass={'build_py': CMakeBuild},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
+
+# python3 setup.py bdist_wheel
+# auditwheel repair -w dist dist/*
+# twine upload dist/*manylinux*
