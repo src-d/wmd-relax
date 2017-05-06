8dfb9aef9bb916bd14c78e25fe33f913dd9b7e0c
Vadim Markovtsev <vadim@sourced.tech>
2017-05-06 10:39:08 +0200

    Bump version 1.2.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ LD_FLAGS = {
 setup(
     name=PACKAGE,
     description="Accelerated functions to calculate Word Mover's Distance",
-    version="1.2.1",
+    version="1.2.2",
     license="MIT",
     author="source{d}",
     author_email="vadim@sourced.tech",
